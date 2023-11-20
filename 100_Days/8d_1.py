46a5c069da1f61304793eaa3d88d7f7b80c1329b
skwwnl <skwwnl@gmail.com>
2023-11-20 09:40:02 +0900

    Python 100 Days - 8d

--- 100_Days/8d_1.py
+++ 100_Days/8d_1.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -18,6 +18,8 @@ def encrypt(plain_text, shift_amount):
   for letter in plain_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
+    if new_position > 25:
+      new_position -= 26
     new_letter = alphabet[new_position]
     cipher_text += new_letter
   print(f"The encoded text is {cipher_text}")
--- 100_Days/8d_2.py
+++ 100_Days/8d_2.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -9,11 +9,13 @@ def encrypt(plain_text, shift_amount):
   for letter in plain_text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
+    if new_position > 25:
+      new_position -= 26
     cipher_text += alphabet[new_position]
   print(f"The encoded text is {cipher_text}")
 
 #TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-def decrypt(cipher_text, shift_amount):
+def decrypt(plain_text, shift_amount):
 
   #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
   #e.g.
@@ -21,7 +23,18 @@ def decrypt(cipher_text, shift_amount):
   #shift = 5
   #plain_text = "hello"
   #print output: "The decoded text is hello"
-
+  decrypt_text = ""
+  cipher_text = plain_text
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    if new_position < 0:
+        new_position += 26
+    decrypt_text += alphabet[new_position]
+  print(f"The decoded text is {decrypt_text}")
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-encrypt(plain_text=text, shift_amount=shift)
+if direction == "encode":
+  encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+  decrypt(plain_text=text, shift_amount=shift)
