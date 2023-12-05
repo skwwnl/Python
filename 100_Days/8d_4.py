0781887a5a4b9be2b5e400840d4be4511e3727c6
skwwnl <skwwnl@gmail.com>
2023-12-05 14:32:37 +0900

    Python 100 Days Update

--- 100_Days/8d_4.py
+++ 100_Days/8d_4.py
@@ -20,23 +20,32 @@ def caesar(start_text, shift_amount, cipher_direction):
     print(f"Here's the {cipher_direction}d result: {end_text}")
 
 
-# TODO-1: Import and print the logo from art.py when the program starts.
-import art
-print(f"{art.logo}")
+# TODO-1: Import and print the logo from mod_8d_caesar_art.py when the program starts.
+import mod_8d_caesar_art
+print(f"{mod_8d_caesar_art.logo}")
 
 # TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 # e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
 # If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
 # Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+a = True
+while a:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    ask = input("If you want to restart the cipher program? Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if ask == "no":
+        a = False
+        print("Goodbye")
 
 # TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
 # Try running the program and entering a shift number of 45.
 # Add some code so that the program continues to work even if the user enters a shift number greater than 26.
 # Hint: Think about how you can use the modulus (%).
-shift = shift % 26
-
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+# shift = shift % 26
+#
+# caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
---
+++ 100_Days/9d_final.py
@@ -0,0 +1,32 @@
+import os
+# HINT: You can call cls in system to clear the output in the console.
+
+from mod_9d_art import logo
+
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    # bidding_record = {"aa": 123, "bb": 321}
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+while not bidding_finished:
+    name = input("What's your name?: ")
+    price = int(input("input your bid: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        os.system('cls')
