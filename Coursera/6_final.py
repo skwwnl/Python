38795ef081b25b2c2948ad4938de963dd1669838
skwwnl <skwwnl@gmail.com>
2024-01-17 23:23:52 +0900

    Python Operation System Module 2 Quiz Update

---
+++ Coursera/6_final.py
@@ -0,0 +1,63 @@
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+# This is the uploader widget
+
+def _upload():
+
+    _upload_widget = fileupload.FileUploadWidget(label='Browse', _dom_classes=('widget_item', 'btn-group'))
+
+    def _cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    _upload_widget.observe(_cb, names='data')
+    display(_upload_widget)
+
+_upload()
+
+
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
+                           "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its",
+                           "they", "them", \
+                           "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be",
+                           "been", "being", \
+                           "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
+                           "where", "how", \
+                           "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very",
+                           "can", "will", "just"]
+
+    # LEARNER CODE START HERE
+    required_dictionary = {}
+    dictionary_list=[]
+    for words in file_contents.split():
+        if words.isalpha() and words.lower() not in uninteresting_words:
+            dictionary_list.append(words.lower())
+    for words in dictionary_list:
+        if words not in required_dictionary:
+            required_dictionary[words] =1
+        required_dictionary[words]+=file_contents.split().count(words)
+
+    #wordcloud
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(required_dictionary)
+    return cloud.to_array()
+
+# Display your wordcloud image
+
+myimage = calculate_frequencies(file_contents)
+plt.imshow(myimage, interpolation = 'nearest')
+plt.axis('off')
+plt.show()
